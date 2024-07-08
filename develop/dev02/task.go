431835462f3da72a5d39bb3377c0f8491145e3e0
vadim <lociX55Lim@gmail.com>
2024-07-08 14:41:26 +0300

    fix: golint

--- develop/dev02/task.go
+++ develop/dev02/task.go
@@ -11,19 +11,23 @@ import (
 
 var lastChar rune
 
+// Chain - Интерфейс наших звеньев цепочки
 type Chain interface {
 	Unpack(r rune, result *strings.Builder) error
 	SetNext(chain Chain)
 }
 
+// BaseChain - базовая реализация наших звеньев
 type BaseChain struct {
 	next Chain
 }
 
+// SetNext - добавления звена в цепочку
 func (bc *BaseChain) SetNext(chain Chain) {
 	bc.next = chain
 }
 
+// Unpack - попытка передачи обработки следующему, если конкретная реализация не смогла распаковать
 func (bc *BaseChain) Unpack(r rune, result *strings.Builder) error {
 	if bc.next == nil {
 		return errors.New("invalid string")
@@ -31,11 +35,13 @@ func (bc *BaseChain) Unpack(r rune, result *strings.Builder) error {
 	return bc.next.Unpack(r, result)
 }
 
-type SymbolChain struct {
+// LetterChain - звено для обычных букв
+type LetterChain struct {
 	BaseChain
 }
 
-func (sc *SymbolChain) Unpack(r rune, result *strings.Builder) error {
+// Unpack - попытка распаковать букву
+func (sc *LetterChain) Unpack(r rune, result *strings.Builder) error {
 	if !unicode.IsLetter(r) {
 		return sc.BaseChain.Unpack(r, result)
 	}
@@ -44,10 +50,12 @@ func (sc *SymbolChain) Unpack(r rune, result *strings.Builder) error {
 	return nil
 }
 
+// DigitChain - звено для цифр
 type DigitChain struct {
 	BaseChain
 }
 
+// Unpack - пытаемся правильно распаковать цифру
 func (dc *DigitChain) Unpack(r rune, result *strings.Builder) error {
 	if !unicode.IsDigit(r) {
 		return dc.BaseChain.Unpack(r, result)
@@ -63,12 +71,14 @@ func (dc *DigitChain) Unpack(r rune, result *strings.Builder) error {
 	return nil
 }
 
+// EscapeChain - звено для escape последовательностей
 type EscapeChain struct {
 	BaseChain
 	pos *int
 	str *[]rune
 }
 
+// Unpack - пытаемся правильно распаковать escape последовательность
 func (ec *EscapeChain) Unpack(r rune, result *strings.Builder) error {
 	if r != 92 {
 		return ec.BaseChain.Unpack(r, result)
@@ -82,12 +92,13 @@ func (ec *EscapeChain) Unpack(r rune, result *strings.Builder) error {
 	return nil
 }
 
+// Unpack - функция, которая получает исходную строку и пытается ее распаковать с помощью цепочки
 func Unpack(str string) (string, error) {
 	var builder strings.Builder
 	runes := []rune(str)
 	var i int
 
-	sc := &SymbolChain{}
+	sc := &LetterChain{}
 	dc := &DigitChain{}
 	ec := &EscapeChain{
 		pos: &i,
